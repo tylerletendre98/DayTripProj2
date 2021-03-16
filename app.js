285f65fe4ab12b2965a160581ce7f9e84c9fa229
T U <tylerletendre98@gmail.com>
2021-03-16 11:28:24 -0400

    added to the functions that the user gets to change what each one of there selections will be based on a yes or no answer

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
+"use strict"
 let transportation = ['car','bike','plane','walking','bus'];
-let destinations = ['chilis','beach','bowling alley', 'concert'];
-let entertainment = ['music','video games','book','movie'];
+let destinations = ['chilis','the beach','a bowling alley', 'a concert'];
+let entertainment = ['listening to music',' playing video games','a book','watching a movie'];
 let dayTrip = []
 
 function getRandomTransportation(array){
-    randomTransportation = array[Math.floor(Math.random()* array.length)];
+    let randomTransportation = array[Math.floor(Math.random()* array.length)];
+    alert(randomTransportation);
+    let userInput = prompt("Please write yes or no to keep this entertainment");
+    while(userInput !== "yes"){
+        randomTransportation = array[Math.floor(Math.random()* array.length)];
+        alert(randomTransportation);
+        userInput = prompt("do you want to keep this entertainment yes or no?");
+        if (userInput== "yes"){
+            dayTrip.push(randomTransportation);
+        }
+    }
     dayTrip.push(randomTransportation);
 }
 function getRandomDestination(array){
-    randomDestination= array[Math.floor(Math.random()* array.length)];
+    let randomDestination = array[Math.floor(Math.random()* array.length)];
+    alert(randomDestination);
+    let userInput = prompt("Please write yes or no to keep this entertainment");
+    while(userInput !== "yes"){
+        randomDestination = array[Math.floor(Math.random()* array.length)];
+        alert(randomDestination);
+        userInput = prompt("do you want to keep this entertainment yes or no?");
+        if (userInput== "yes"){
+            dayTrip.push(randomDestination);
+        }
+    }
     dayTrip.push(randomDestination);
 }
 function getRandomEntertainment(array){
-    randomEntertainment = array[Math.floor(Math.random()* array.length)];
+    let randomEntertainment = array[Math.floor(Math.random()* array.length)];
+    alert(randomEntertainment);
+    let userInput = prompt("Please write yes or no to keep this entertainment");
+    while(userInput !== "yes"){
+        randomEntertainment = array[Math.floor(Math.random()* array.length)];
+        alert(randomEntertainment);
+        userInput = prompt("do you want to keep this entertainment yes or no?");
+        if (userInput== "yes"){
+            dayTrip.push(randomEntertainment);
+        }
+    }
     dayTrip.push(randomEntertainment);
 }
-
-
-
-
-
-
-
-
 getRandomTransportation(transportation);
 getRandomDestination(destinations);
 getRandomEntertainment(entertainment);
-console.log(dayTrip);
+console.log("I will be using a"+ " "+dayTrip[0]+" my destination will be"+" "+dayTrip[1]+" my form of entertainment will be"+" "+dayTrip[2]+".");
