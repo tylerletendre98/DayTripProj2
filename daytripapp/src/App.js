358931f75a5fbe9a6fb7ac9ab866603fd4b5986a
T U <tylerletendre98@gmail.com>
2021-10-10 10:45:05 -0400

    added bootstrap and arrays for the different type of things for the daytrip

--- daytripapp/src/App.js
+++ daytripapp/src/App.js
@@ -2,7 +2,29 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  state = {
+    modesOfTransportation: [
+      "driving a car",
+      "riding a bike",
+      "flying a plane",
+      "walking",
+      "riding the bus",
+    ],
+    differentDestinations: ["the beach", "a bowling alley", "a concert"],
+    typesOfEntertainment: [
+      "listening to music",
+      " playing video games",
+      "reading a book",
+      "watching a movie",
+    ],
+    differentRestaurants: [
+      "chilis",
+      "buffalo wild wings",
+      "TGI Fridays",
+      "Wendys",
+      "Ihop",
+    ],
+  };
 
   render() {
     return (
