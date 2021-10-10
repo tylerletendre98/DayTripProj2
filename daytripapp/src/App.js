3defe4e3ddaf791cb5986004b6e61824b6ff63da
T U <tylerletendre98@gmail.com>
2021-10-10 10:37:39 -0400

    added in the react app to move this to visual website rather then the console

---
+++ daytripapp/src/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="App">
+        <h1>day trip app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
